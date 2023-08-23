1e9396b6ba7413f136a0729e79ebbafeebb8b1b5
Mathias Nordlander <mnordlander@ct-group.com>
2023-08-23 14:07:56 +0200

    New effect function for mobile resize

--- responsive-landscape/src/App.js
+++ responsive-landscape/src/App.js
@@ -1,11 +1,46 @@
-import React from 'react';
-import Page from "./components/Page";
+import React, { useState, useEffect } from 'react';
 import "./index.css";
+import Player from './components/Player';
+import Chat from './components/Chat';
 
 function App() {
+  const [mobileLandscape, setMobileLandscape] = useState(false)
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  })
+
+  function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  }
+
+  console.log(isMobileDevice());
+
+  useEffect(() => {
+    function handleResize() {
+      const newDimensions = {
+        height: window.innerHeight,
+        width: window.innerWidth
+      };
+      setDimensions(newDimensions);
+
+      setMobileLandscape(newDimensions.width > newDimensions.height);
+
+    }
+
+    window.addEventListener('resize', handleResize)
+
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
-    <div className="body-wrapper">
-      <Page />
+    <div className="wrapper">
+      <Player width={dimensions.width} height={dimensions.height} />
+      <Chat />
+      {mobileLandscape ? <p>Mobile is in landscape mode</p> : <p>Mobile is not in landscape mode</p>}
     </div>
   );
 }
